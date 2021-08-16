775e79e8633e4c0f71aa352ed953542d1c8ced17
OMRYZUTA <omrizu@mta.ac.il>
2021-08-16 10:58:28 +0300

    background send and gets back messages

--- background.js
+++ background.js
@@ -1,54 +1,96 @@
-URL = "http://127.0.0.1:8000/"
 
-// Example POST method implementation:
-async function postData(url = '', data = {}) {
-    // Default options are marked with *
-    const response = await fetch(url, {
-      method: 'POST', // *GET, POST, PUT, DELETE, etc.
-      //mode: 'no-cors', // no-cors, *cors, same-origin
-      //cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-      //credentials: 'same-origin', // include, *same-origin, omit
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-        // 'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      //redirect: 'follow', // manual, *follow, error
-      //referrerPolicy: 'same-origin', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-      body: JSON.stringify(data) // body data type must match "Content-Type" header
-    });
-    return response.json(); // parses JSON response into native JavaScript objects
-}
-  
-  
-
-chrome.runtime.onInstalled.addListener((details)=>{
-    chrome.contextMenus.create({
-        title: "save position",
-        id: "contextMenu1",
-        contexts:["page","selection"]
-    });
-    chrome.contextMenus.onClicked.addListener((event)=>
-    {
-        let selectionValues = event.selectionText.split("-");
-
-        let jobProcess ={};
-        
-        jobProcess["position"] ={};
-        jobProcess["contact_set"]=[];
-        jobProcess["stage_set"]=[];
-        jobProcess["status"]={"name":"Interested"}
-        jobProcess.position["job_posting_URL"]= event.pageUrl;
-        jobProcess.position["job_title"]= selectionValues[0];
-        let restOfTheString =selectionValues[1].split(" ");
-        jobProcess.position["company_name"] = restOfTheString[0];
-        jobProcess.position["city"] = restOfTheString[1];
-        console.log(jobProcess);
-        postData(URL, jobProcess)
-    .then(data => {
-      console.log(data); // JSON data parsed by `data.json()` call
-    }).catch(err => {
-        console.log(err);
-    });
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  if (changeInfo.status === 'complete') {
+    if (String(tab.url).includes('indeed')) {
+      console.log(tab.url);
+      chrome.tabs.sendMessage(tabId, { message: "hi" },
+        response => {
+          console.log(response);
+          chrome.storage.sync.set({ "position": response },
+            function () { console.log(" value is set to " + response); }
+          );
+          let position = {}
+          chrome.storage.sync.get(['position'], function (result) {
+            console.log(result);
+            position = result;
+          });
+          chrome.runtime.sendMessage({ msg: "fetched_position" },
+            position )
+        }
+      )
     }
-)});
+  }
+}
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// URL = "http://127.0.0.1:8000/"
+
+// // Example POST method implementation:
+// async function postData(url = '', data = {}) {
+//     // Default options are marked with *
+//     const response = await fetch(url, {
+//       method: 'POST', // *GET, POST, PUT, DELETE, etc.
+//       //mode: 'no-cors', // no-cors, *cors, same-origin
+//       //cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+//       //credentials: 'same-origin', // include, *same-origin, omit
+//       headers: {
+//         'Accept': 'application/json',
+//         'Content-Type': 'application/json'
+//         // 'Content-Type': 'application/x-www-form-urlencoded',
+//       },
+//       //redirect: 'follow', // manual, *follow, error
+//       //referrerPolicy: 'same-origin', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+//       body: JSON.stringify(data) // body data type must match "Content-Type" header
+//     });
+//     return response.json(); // parses JSON response into native JavaScript objects
+// }
+
+
+
+// chrome.runtime.onInstalled.addListener((details)=>{
+//     chrome.contextMenus.create({
+//         title: "save position",
+//         id: "contextMenu1",
+//         contexts:["page","selection"]
+//     });
+//     chrome.contextMenus.onClicked.addListener((event)=>
+//     {
+//         let selectionValues = event.selectionText.split("-");
+
+//         let jobProcess ={};
+
+//         jobProcess["position"] ={};
+//         jobProcess["contact_set"]=[];
+//         jobProcess["stage_set"]=[];
+//         jobProcess["status"]={"name":"Interested"}
+//         jobProcess.position["job_posting_URL"]= event.pageUrl;
+//         jobProcess.position["job_title"]= selectionValues[0];
+//         let restOfTheString =selectionValues[1].split(" ");
+//         jobProcess.position["company_name"] = restOfTheString[0];
+//         jobProcess.position["city"] = restOfTheString[1];
+//         console.log(jobProcess);
+//         postData(URL, jobProcess)
+//     .then(data => {
+//       console.log(data); // JSON data parsed by `data.json()` call
+//     }).catch(err => {
+//         console.log(err);
+//     });
+//     }
+// )});
--- contentScript.js
+++ contentScript.js
@@ -1,17 +1,23 @@
-
-window.addEventListener("load", scrape, false);
-console.log("in content script");
-
+chrome.runtime.onMessage.addListener((msg, sender, sendResopnse) => {
+    console.log("in content script", msg, sender, sendResopnse);
+    sendResopnse(scrape());
+})
 const scrape = () => {
     let jobTitle = "";
     let url = String(window.location.href);
-
+    let position = {};
     if (url.includes("indeed")) {
         console.log("in indeed.com !");
-        if (url.includes("vjk")) {
+        if (url.includes("vjk") || url.includes("vjs")) {
             console.log("job title array is :" + document.getElementsByClassName("jobsearch-JobInfoHeader-title")[0])
             jobTitle = document.getElementsByClassName("jobsearch-JobInfoHeader-title")[0]?.innerText.replace("- job post", "")
             console.log("job title: ", jobTitle);
+            position = { jobTitle };
+            companyName = document.getElementsByClassName("jobsearch-InlineCompanyRating")[0].innerText;
+            console.log(companyName);
+            position = { companyName, jobTitle, url };
+            console.log('position: ', position);
         }
     }
+    return position;
 }

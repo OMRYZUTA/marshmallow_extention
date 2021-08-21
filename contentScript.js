4103c10f04bbe191e64a7824482d3b78d36d244f
OMRYZUTA <omrizu@mta.ac.il>
2021-08-21 19:00:04 +0300

    working scraping on indeed

--- contentScript.js
+++ contentScript.js
@@ -1,23 +1,33 @@
 chrome.runtime.onMessage.addListener((msg, sender, sendResopnse) => {
-    console.log("in content script", msg, sender, sendResopnse);
     sendResopnse(scrape());
 })
+let jobTitle = "";
+let url = String(window.location.href);
+let position = {};
+
 const scrape = () => {
-    let jobTitle = "";
-    let url = String(window.location.href);
-    let position = {};
     if (url.includes("indeed")) {
-        console.log("in indeed.com !");
         if (url.includes("vjk") || url.includes("vjs")) {
-            console.log("job title array is :" + document.getElementsByClassName("jobsearch-JobInfoHeader-title")[0])
-            jobTitle = document.getElementsByClassName("jobsearch-JobInfoHeader-title")[0]?.innerText.replace("- job post", "")
-            console.log("job title: ", jobTitle);
-            position = { jobTitle };
-            companyName = document.getElementsByClassName("jobsearch-InlineCompanyRating")[0].innerText;
-            console.log(companyName);
-            position = { companyName, jobTitle, url };
-            console.log('position: ', position);
+            jobTitle = document.getElementsByClassName("jobsearch-JobInfoHeader-title")[0]?.innerText.replace("- job post", "");
+            if (typeof jobTitle === "undefined") {
+                jobTitle = document.getElementById('vjs-jobtitle').innerText
+                companyName = document.getElementById("vjs-cn").innerText;
+                let url = String(window.location.href);
+                position = { companyName, jobTitle, url };
+                console.log('position: ', position);
+            }
+            else {
+                position = { jobTitle };
+                companyName = document.getElementsByClassName("jobsearch-InlineCompanyRating")[0].innerText;
+                console.log(companyName);
+                let url = String(window.location.href);
+                position = { companyName, jobTitle, url };
+                console.log('position: ', position);
+            }
         }
+    }
+    else if (url.includes("linkedin")) {
+        console.log("in linkedin");
     }
     return position;
 }
--- popup/popup.js
+++ popup/popup.js
@@ -8,8 +8,8 @@ chrome.storage.sync.get(['position'], function (positionWrapper) {
     console.log(document.getElementById("checkbox-link"))
     document.getElementById("checkbox-link").checked = true;
     console.log(document.getElementById("job-title"));
-    document.getElementById("job-title").value += position.jobTitle;
-    document.getElementById("company-name").value += position.companyName;
+    document.getElementById("job-title").value = position.jobTitle || '';
+    document.getElementById("company-name").value = position.companyName || '';
 
 })
 document.getElementById("save-position-btn").addEventListener("click", function (e) {
